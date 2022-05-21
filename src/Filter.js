fa53d8ac85c3e28c0fd4d453f2334bd0b293afee
Kaito <74711118+sc30gsw@users.noreply.github.com>
2022-05-22 06:28:01 +0900

    fiter関数の使用法

--- src/Filter.js
+++ src/Filter.js
@@ -1,9 +1,23 @@
 import "./styles.css";
 
 export default function Filter() {
+  const words = [
+    "spray",
+    "limit",
+    "elite",
+    "exuberant",
+    "destruction",
+    "present"
+  ];
+
+  const result = words.filter((word) => word.length < 6);
+
   return (
     <div className="Filter">
       <h1>Filter</h1>
+      {/* 条件を満たした要素だけを残す関数 */}
+      {/* 条件を満たさない要素は削除される */}
+      <div>{result}</div>
     </div>
   );
 }
