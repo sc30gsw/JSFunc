ddd78fc15b1312adf897a93a304043f4098e2a0f
Kaito <74711118+sc30gsw@users.noreply.github.com>
2022-05-22 06:03:12 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./styles.css";
+
+export default function App() {
+  const list = [1, 2, 3, 4];
+  const dataList = [
+    {
+      name: "taro",
+      age: 21
+    },
+    {
+      name: "jiro",
+      age: 32
+    }
+  ];
+  return (
+    <div className="App">
+      <h1>map-practice</h1>
+      {list.map((num) => (
+        <div>{num * 2}</div>
+      ))}
+      {dataList.map((data) => (
+        <div>
+          {data.name}
+          <span>{data.age}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
