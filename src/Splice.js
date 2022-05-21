866c93c806b4147a7cea3754bd04fa281a08736b
Kaito <74711118+sc30gsw@users.noreply.github.com>
2022-05-22 06:39:51 +0900

    Splice関数

--- src/Splice.js
+++ src/Splice.js
@@ -1,6 +1,13 @@
 import "./styles.css";
 
 export default function Splice() {
+  const items = ["item1", "item2", "item3"];
+  const remove = items.splice(0, 1);
+  console.log(remove);
+
+  items.splice(1, 0, remove[0]);
+  console.log(items);
+  /* splice(start, deleteCount, item1) */
   return (
     <div className="Splice">
       <h1>Splice</h1>
