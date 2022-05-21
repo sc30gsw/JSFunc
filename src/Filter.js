bf978b4fb706ea2345db46e701b3ee296b2e626e
Kaito <74711118+sc30gsw@users.noreply.github.com>
2022-05-22 06:17:35 +0900

    Filterが読み込まれるように変更

--- src/Filter.js
+++ src/Filter.js
@@ -1,29 +1,9 @@
 import "./styles.css";
 
-export default function App() {
-  const list = [1, 2, 3, 4];
-  const dataList = [
-    {
-      name: "taro",
-      age: 21
-    },
-    {
-      name: "jiro",
-      age: 32
-    }
-  ];
+export default function Filter() {
   return (
-    <div className="App">
-      <h1>map</h1>
-      {list.map((num) => (
-        <div>{num * 2}</div>
-      ))}
-      {dataList.map((data) => (
-        <div>
-          {data.name}
-          <span>{data.age}</span>
-        </div>
-      ))}
+    <div className="Filter">
+      <h1>Filter</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import Map from "./Map";
+import Filter from "./Filter";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Map />
+    <Filter />
   </StrictMode>
 );
